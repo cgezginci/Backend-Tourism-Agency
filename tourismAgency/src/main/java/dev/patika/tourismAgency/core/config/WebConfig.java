6fc038c19900c1a51b637e291a5743090ea9759b
cgezginci <cgezginci76@gmail.com>
2024-04-01 14:47:03 +0300

    Project Updated

--- tourismAgency/src/main/java/dev/patika/tourismAgency/core/config/WebConfig.java
+++ tourismAgency/src/main/java/dev/patika/tourismAgency/core/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://helpful-sundae-1cb4c5.netlify.app/")
                 .allowedMethods("GET","POST","PUT","DELETE")
                 .allowCredentials(true);
     }
